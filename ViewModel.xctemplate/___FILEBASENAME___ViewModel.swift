1636573c1bf9bcfdd3ab486dd09da12cf4d67c55
Batuhan Arda <batuardahan22@gmail.com>
2025-08-17 16:05:51 +0300

    Add custom ViewModel Template

---
+++ ViewModel.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,17 @@
+___FILEHEADER___
+
+
+import Foundation
+
+@MainActor
+final class ___VARIABLE_productName:identifier___ViewModel: ObservableObject {
+    
+    // MARK: - Init
+    init() {}
+    
+    // MARK: - State Properties
+    // @Published var example: String = ""
+    
+    // MARK: - Functions
+    // func fetchData() async {}
+}
